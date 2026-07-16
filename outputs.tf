d80a97d55f1fed18f584ff9f7663ab5f374989a0
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:02:33 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "repository_topicses_id" {
   description = "Map of id values across all repository_topicses, keyed the same as var.repository_topicses"
-  value       = { for k, v in github_repository_topics.repository_topicses : k => v.id }
+  value       = { for k, v in github_repository_topics.repository_topicses : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "repository_topicses_repository" {
   description = "Map of repository values across all repository_topicses, keyed the same as var.repository_topicses"
-  value       = { for k, v in github_repository_topics.repository_topicses : k => v.repository }
+  value       = { for k, v in github_repository_topics.repository_topicses : k => v.repository if v.repository != null && length(v.repository) > 0 }
 }
 output "repository_topicses_topics" {
   description = "Map of topics values across all repository_topicses, keyed the same as var.repository_topicses"
-  value       = { for k, v in github_repository_topics.repository_topicses : k => v.topics }
+  value       = { for k, v in github_repository_topics.repository_topicses : k => v.topics if v.topics != null && length(v.topics) > 0 }
 }
 
